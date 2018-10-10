a7697d7828cc564a98bb5eb864aad33aa94b6518
modulus100 <aleksandr.tserepannikov@tptlive.ee>
2018-10-10 20:25:23 +0300

    canderra distance added

---
+++ canderraDistance.R
@@ -0,0 +1,11 @@
+canderraDistance <- function(v1, v2) {
+  #https://en.wikipedia.org/wiki/Canberra_distance
+  sum <- 0
+  
+  for (i in 1:length(v1)) {
+    tempVal <- (abs(v1[i] - v2[i])) / (abs(v1[i]) - abs(v2[i]))
+    sum <- sum + tempVal
+  }
+  #return sum
+  sum
+}
