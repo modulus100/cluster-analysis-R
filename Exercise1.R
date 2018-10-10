d92c5fa41010c164157e10231e6dc09c837086c6
modulus100 <aleksandr.tserepannikov@tptlive.ee>
2018-10-10 19:54:11 +0300

    vectors removed

--- Exercise1.R
+++ Exercise1.R
@@ -6,7 +6,4 @@ source("minkovskiDistance.R")
 #x - generated matrix
 distance <- mahalanobisDistance(x[2,],x[4,], x)
 
-v1 <- c(0, 3, 4, 5)
-v2 <- c(7, 6, 3, -1)
-
 distance2 <- minkovskiDistance(x[2,],x[4,], 5)
