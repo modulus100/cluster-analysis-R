5cc82a3deb31d01603f695829a4c381bfbc3a6b3
modulus100 <aleksandr.tserepannikov@tptlive.ee>
2018-10-17 14:54:24 +0300

    comments added

--- computeNewCentroids.R
+++ computeNewCentroids.R
@@ -4,16 +4,19 @@ computeNewCentroids <- function(dataset, k) {
   dataDimension <- dim(dataset)[2]
   newCentroids <- matrix(0, k, dataDimension)
   
+  # go trought all the dataset
   while(cnt < k + 1) {
     sumVector <- numeric(dataDimension - 1)
     indexSamples <- 1
     dataCount <- 0
     
+    # calculate number of samples for each k 
     while (indexSamples < numSamples + 1) {
       if (dataset[indexSamples,][dataDimension] == cnt) {
         dataCount <- dataCount + 1
         indexDimension <- 1
         
+        # calculate sum for each row in the dataset
         while(indexDimension < dataDimension) {
           sumVector[indexDimension] = sumVector[indexDimension] + dataset[indexSamples,][indexDimension]
           indexDimension <- indexDimension + 1
@@ -25,6 +28,7 @@ computeNewCentroids <- function(dataset, k) {
     
     indexDimension <- 1
     
+    # calculate mean value for each row of filtered row
     while(indexDimension < dataDimension) {
       # means, last columns is ignored
       newCentroids[cnt,][indexDimension] <- sumVector[indexDimension] / dataCount
